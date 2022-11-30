5dafa8b350c99b6dc4dcfcf5ba0d13147c6705e6
khshohag99 <khondoker.aminuzzaman@gmail.com>
2022-11-30 18:31:19 -0500

    Developed Dashboard drilldown features For Heart Rate

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:swastha/moreInfo.dart';
+import 'package:swastha/moreInfoHeart.dart';
 import 'package:swastha/moreInfoSleeps.dart';
 import 'package:swastha/moreInfoSpo2.dart';
 import 'package:swastha/moreInfoSteps.dart';
@@ -98,7 +99,7 @@ class _DashboardState extends State<Dashboard> {
         .doc(id)
         .collection("ACTIVE_ENERGY_BURNED");
     final calorieData =
-        await calorieRef.orderBy('date_from', descending: true).limit(1).get();
+    await calorieRef.orderBy('date_from', descending: true).limit(1).get();
     calorieData.docs.forEach((doc) {
       double value = double.parse(doc["value"]);
       value = value / 1000;
@@ -115,7 +116,7 @@ class _DashboardState extends State<Dashboard> {
         .doc(id)
         .collection("STEPS");
     final stepData =
-        await stepsRef.orderBy('date_from', descending: true).limit(1).get();
+    await stepsRef.orderBy('date_from', descending: true).limit(1).get();
     stepData.docs.forEach((doc) {
       // int value = int.parse(doc["value"]);
       //double val = value * 1000;
@@ -131,7 +132,7 @@ class _DashboardState extends State<Dashboard> {
         .doc(id)
         .collection("HEART_RATE");
     final heartdata =
-        await ref.orderBy('date_from', descending: true).limit(1).get();
+    await ref.orderBy('date_from', descending: true).limit(1).get();
 
     heartdata.docs.forEach((doc) {
       double value = double.parse(doc["value"]);
@@ -148,7 +149,7 @@ class _DashboardState extends State<Dashboard> {
         .doc(id)
         .collection("SLEEP_IN_BED");
     final sleepData =
-        await sleepRef.orderBy('date_from', descending: true).limit(1).get();
+    await sleepRef.orderBy('date_from', descending: true).limit(1).get();
     sleepData.docs.forEach((doc) {
       double value = double.parse(doc["value"]);
       print("sleepValue " + value.toString());
@@ -172,7 +173,7 @@ class _DashboardState extends State<Dashboard> {
         .doc(id)
         .collection("WEIGHT");
     final weightData =
-        await weightRef.orderBy('date_from', descending: true).limit(1).get();
+    await weightRef.orderBy('date_from', descending: true).limit(1).get();
     weightData.docs.forEach((doc) {
       double value = double.parse(doc["value"]);
       print("weightValue " + value.toString());
@@ -189,7 +190,7 @@ class _DashboardState extends State<Dashboard> {
         .doc(id)
         .collection("HEIGHT");
     final heightData =
-        await heightRef.orderBy('date_from', descending: true).limit(1).get();
+    await heightRef.orderBy('date_from', descending: true).limit(1).get();
     heightData.docs.forEach((doc) {
       double height = double.parse(doc["value"]);
       print("heightValue " + height.toString());
@@ -207,7 +208,7 @@ class _DashboardState extends State<Dashboard> {
         .doc(id)
         .collection("BLOOD_OXYGEN");
     final spo2Data =
-        await spo2Ref.orderBy('date_from', descending: true).limit(1).get();
+    await spo2Ref.orderBy('date_from', descending: true).limit(1).get();
     spo2Data.docs.forEach((doc) {
       double value = double.parse(doc["value"]);
       print("Spo2Value " + value.toString());
@@ -220,6 +221,56 @@ class _DashboardState extends State<Dashboard> {
     });
   }
 
+  // fetchCalorie() {
+  //   StreamBuilder<QuerySnapshot>(
+  //       stream: FirebaseFirestore.instance
+  //           .collection("data")
+  //           .doc(id)
+  //           .collection(id)
+  //           // .orderBy("timestamp", descending: true)
+  //           // .limit(10)
+  //           .snapshots(),
+  //       builder: (context, snapshot) {
+  //         if (!snapshot.hasData) {
+  //           print("Snapshot is empty");
+  //         } else {
+  //           calorie = snapshot.data!.docs[2]["value"];
+  //           // final values = snapshot.data!.docs.reversed;
+  //           //
+  //           // for (var value in values) {
+  //           //   final message = value.data();
+  //           //   print("from dashboard:" + message.toString());
+  //           // }
+  //
+  //           return Text("");
+  //         }
+  //
+  //         setState(() {
+  //           calorie = snapshot.data!.docs[0]["value"];
+  //         });
+  //         return Text("");
+  //       });
+  // }
+
+  // Future<dynamic> getCalorie() async {
+  //   dynamic val = "";
+  //   final CollectionReference ref =
+  //       FirebaseFirestore.instance.collection("data").doc(id).collection(id);
+  //   // QuerySnapshot snapshot =
+  //   //     await ref.orderBy("timestamp", descending: true).limit(1).get();
+  //   await ref
+  //       .orderBy('timestamp', descending: true)
+  //       .limit(1)
+  //       .get()
+  //       .then((value) async {
+  //     val = value;
+  //   });
+  //
+  //   setState(() {
+  //     calorie = val["value"].toString();
+  //   });
+  //   print("From Dashboard:" + calorie);
+  // }
   //Pull to Refresh
   Future<void> _pullRefresh() async {
     HealthApp healthobj = new HealthApp();
@@ -232,7 +283,7 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:
-          kBackgroundColor, //const Color(0xFF0A0D22), //Color(0xff181C1F),
+      kBackgroundColor, //const Color(0xFF0A0D22), //Color(0xff181C1F),
       appBar: AppBar(
         backgroundColor: Color(0xFF0A0D22), //const Color(0xff181C1F),
         title: const Text(
@@ -247,7 +298,7 @@ class _DashboardState extends State<Dashboard> {
           GestureDetector(
             child: Container(
               margin:
-                  const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+              const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
               child: CircleAvatar(
                 radius: 20.0,
                 backgroundImage: NetworkImage(photoUrl),
@@ -303,59 +354,67 @@ class _DashboardState extends State<Dashboard> {
                 Row(
                   children: [
                     //Heart Rate Card
-                    Container(
-                      margin: const EdgeInsets.all(10.0),
-                      height: 250.0,
-                      width: 160.0,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(40.0),
-                        color: kWeightCardColor,
-                        //const Color(0xFF7A85F8), //Color(0xFF1D1E33), //0xFF0A0E21
-                      ),
-                      child: Column(
-                        children: [
-                          const SizedBox(
-                            height: 20.0,
-                          ),
-                          const CircleAvatar(
-                            radius: 35.0,
-                            backgroundColor: Color(0xFF292C31),
-                            child: FaIcon(
-                              FontAwesomeIcons.heartPulse,
-                              size: 30.0,
-                              color: Colors.red,
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MoreInfoHeart()));
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.all(10.0),
+                        height: 250.0,
+                        width: 160.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(40.0),
+                          color: kWeightCardColor,
+                          //const Color(0xFF7A85F8), //Color(0xFF1D1E33), //0xFF0A0E21
+                        ),
+                        child: Column(
+                          children: [
+                            const SizedBox(
+                              height: 20.0,
                             ),
-                          ),
-                          const SizedBox(
-                            height: 15.0,
-                          ),
-                          // const Text(
-                          //   "Heart Rate",
-                          //   style: TextStyle(
-                          //     fontSize: 22.0,
-                          //     fontWeight: FontWeight.w900,
-                          //     fontFamily: 'Righteous',
-                          //   ),
-                          // ),
-                          const SizedBox(
-                            height: 10.0,
-                          ),
-                          heartRate == ""
-                              ? circularProgressWhite()
-                              : Text(
-                                  heartRate,
-                                  style: const TextStyle(
-                                    fontSize: 40.0,
-                                    fontWeight: FontWeight.w900,
-                                    fontFamily: 'Righteous',
-                                  ),
-                                ),
-                          const Text(
-                            "bpm",
-                            style: TextStyle(
-                                fontSize: 18.0, fontWeight: FontWeight.bold),
-                          ),
-                        ],
+                            const CircleAvatar(
+                              radius: 35.0,
+                              backgroundColor: Color(0xFF292C31),
+                              child: FaIcon(
+                                FontAwesomeIcons.heartPulse,
+                                size: 30.0,
+                                color: Colors.red,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 15.0,
+                            ),
+                            // const Text(
+                            //   "Heart Rate",
+                            //   style: TextStyle(
+                            //     fontSize: 22.0,
+                            //     fontWeight: FontWeight.w900,
+                            //     fontFamily: 'Righteous',
+                            //   ),
+                            // ),
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            heartRate == ""
+                                ? circularProgressWhite()
+                                : Text(
+                              heartRate,
+                              style: const TextStyle(
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w900,
+                                fontFamily: 'Righteous',
+                              ),
+                            ),
+                            const Text(
+                              "bpm",
+                              style: TextStyle(
+                                  fontSize: 18.0, fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
 
@@ -378,7 +437,7 @@ class _DashboardState extends State<Dashboard> {
                                   quarterTurns: 3,
                                   child: CircleAvatar(
                                     backgroundColor:
-                                        Colors.indigo, //Color(0xFF2E8BC0),
+                                    Colors.indigo, //Color(0xFF2E8BC0),
                                     radius: 30.0,
                                     child: FaIcon(
                                       FontAwesomeIcons.shoePrints,
@@ -393,9 +452,9 @@ class _DashboardState extends State<Dashboard> {
                                     steps == ""
                                         ? circularProgressWhite()
                                         : Text(
-                                            steps,
-                                            style: kHorizontalCardText,
-                                          ),
+                                      steps,
+                                      style: kHorizontalCardText,
+                                    ),
                                     const Text(
                                       "Steps Walked",
                                       style: TextStyle(
@@ -433,9 +492,9 @@ class _DashboardState extends State<Dashboard> {
                                   calorie == ""
                                       ? circularProgressWhite()
                                       : Text(
-                                          calorie,
-                                          style: kHorizontalCardText,
-                                        ),
+                                    calorie,
+                                    style: kHorizontalCardText,
+                                  ),
                                   Text(
                                     "kcal burnt",
                                     style: TextStyle(
@@ -488,12 +547,12 @@ class _DashboardState extends State<Dashboard> {
                                 sp02 == ""
                                     ? circularProgressWhite()
                                     : Text(
-                                        sp02,
-                                        style: const TextStyle(
-                                          fontSize: 28.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
+                                  sp02,
+                                  style: const TextStyle(
+                                    fontSize: 28.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ],
                             ),
                           ),
@@ -551,12 +610,12 @@ class _DashboardState extends State<Dashboard> {
                                 bmi == ""
                                     ? circularProgressWhite()
                                     : Text(
-                                        bmi,
-                                        style: const TextStyle(
-                                          fontSize: 28.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
+                                  bmi,
+                                  style: const TextStyle(
+                                    fontSize: 28.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ],
                             ),
                           ),
@@ -616,12 +675,12 @@ class _DashboardState extends State<Dashboard> {
                                 sleepDuration == ""
                                     ? circularProgressWhite()
                                     : Text(
-                                        sleepDuration,
-                                        style: const TextStyle(
-                                          fontSize: 28.0,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
+                                  sleepDuration,
+                                  style: const TextStyle(
+                                    fontSize: 28.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
                               ],
                             ),
                           ),
@@ -692,7 +751,7 @@ class _DashboardState extends State<Dashboard> {
                               radius: 50.0,
                               child: CircleAvatar(
                                 backgroundColor:
-                                    Colors.white, //Color(0xFF78ADFD),
+                                Colors.white, //Color(0xFF78ADFD),
                                 radius: 35.0,
                                 child: FaIcon(
                                   FontAwesomeIcons.virusCovid,
